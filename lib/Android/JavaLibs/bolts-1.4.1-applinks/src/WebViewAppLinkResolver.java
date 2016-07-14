e4808a02f0ae37c3db96448916c9d2e25a50c21c
Robert Segal <rob@getsetgames.com>
2016-07-14 02:26:06 -0400

    Should resolve syntax build error

--- lib/Android/JavaLibs/bolts-1.4.1-applinks/src/WebViewAppLinkResolver.java
+++ lib/Android/JavaLibs/bolts-1.4.1-applinks/src/WebViewAppLinkResolver.java
@@ -125,7 +125,7 @@ public class WebViewAppLinkResolver implements AppLinkResolver {
       @Override
       public Task<JSONArray> then(Task<Void> task) throws Exception {
         // Load the content in a WebView and use JavaScript to extract the meta tags.
-        final TaskCompletionSource<JSONArray> tcs = new TaskCompletionSource<>();
+        final TaskCompletionSource<JSONArray> tcs = new TaskCompletionSource<JSONArray>();
         final WebView webView = new WebView(context);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setNetworkAvailable(false);
