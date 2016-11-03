462f6fa545a5189dbe5156b3634ef72af12ae036
Derek van Vliet <derek@getsetgames.com>
2016-11-03 09:31:25 -0400

    running Facebook login success callback on the game thread

--- Source/FacebookParse/Classes/FacebookLoginComponent.h
+++ Source/FacebookParse/Classes/FacebookLoginComponent.h
@@ -43,7 +43,13 @@ public:
 private:
 	void ApplicationOpenURL_Handler(FString URL, FString SourceApplication);
 	
-	void FacebookLoginSucceeded_Handler(FString UserId, FString AccessToken, FString TokenExpirationDate) { FacebookLoginSucceeded.Broadcast(UserId, AccessToken, TokenExpirationDate); }
+	void FacebookLoginSucceeded_Handler(FString UserId, FString AccessToken, FString TokenExpirationDate)
+	{
+		FGraphEventRef EnterBackgroundTask = FFunctionGraphTask::CreateAndDispatchWhenReady([&]()
+		{
+			FacebookLoginSucceeded.Broadcast(UserId, AccessToken, TokenExpirationDate);
+		}, TStatId(), NULL, ENamedThreads::GameThread);
+	}
 	void FacebookLoginCancelled_Handler() { FacebookLoginCancelled.Broadcast(); }
 	void FacebookLoginError_Handler(FString Error) { FacebookLoginError.Broadcast(Error); }
 
